dc267b5d18a97f494d0ea9fce455b57b4088e94c
elliot-stenhammar-nti-johanneberg <elliot.markstromstenhammar@elev.ga.ntig.se>
2022-01-27 09:19:47 +0100

    lektion 1

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello world!")
+from PIL import Image
+
+def importImage(image):
+    img = Image.open(image)
+    return img
+
+def greyscaleImage(img):
+    img_grey = img.convert('L')
+    return img_grey
+
+def resizeImage(img):
+    width = 240
+    height = 240
+    resized_img = img.resize((width, height), Image.ANTIALIAS)
+    return resized_img
+
+
+left = 0
+top = 0
+right = 80
+bottom = 80
+
+img = resizeImage(importImage("pog.png"))
+
+img1 = img.crop((left, top, right, bottom))
+
+img1.show()
